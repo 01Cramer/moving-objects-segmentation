ac6ab89727a61ae6b29a8d4cb4864518e11e120c
01Cramer <adrian.sliwinski@student.put.poznan.pl>
2023-08-30 08:53:55 +0200

    Update moving_objects_segmentation.py

--- moving_objects_segmentation.py
+++ moving_objects_segmentation.py
@@ -1,7 +1,6 @@
-# basic setup:
+# basic setup: 
 import torch, torchvision, detectron2
 from detectron2.utils.logger import setup_logger
-
 setup_logger()
 
 # import common libraries
@@ -20,22 +19,21 @@ from detectron2.utils.visualizer import ColorMode, Visualizer
 from detectron2.data import MetadataCatalog
 from detectron2.structures import Boxes
 from detectron2.projects import point_rend
+import time
 
 ################# ADDITIONAL FUNCTIONS ##################
 
+def convert(outputs): # easier access (syntax) to detected objects 
+  cls = outputs['instances'].pred_classes
+  masks = outputs['instances'].pred_masks
+  boxes = outputs['instances'].pred_boxes
 
-def convert(outputs):  # easier access (syntax) to detected objects
-    cls = outputs["instances"].pred_classes
-    masks = outputs["instances"].pred_masks
-    boxes = outputs["instances"].pred_boxes
-
-    # create new instance obj and set its fields
-    obj = detectron2.structures.Instances(image_size=(height, width))
-    obj.set("pred_classes", cls)
-    obj.set("pred_masks", masks)
-    obj.set("pred_boxes", boxes)
-    return obj
-
+  # create new instance obj and set its fields
+  obj = detectron2.structures.Instances(image_size=(height, width))
+  obj.set('pred_classes', cls)
+  obj.set('pred_masks', masks)
+  obj.set('pred_boxes', boxes)
+  return obj
 
 def remove_given_indxes(outputs, indx_to_remove):
     cls = outputs.pred_classes
@@ -54,15 +52,14 @@ def remove_given_indxes(outputs, indx_to_remove):
 
     # create new instance obj and set its fields
     obj = detectron2.structures.Instances(image_size=(height, width))
-    obj.set("pred_classes", cls)
-    obj.set("pred_masks", masks)
-    obj.set("pred_boxes", boxes)
+    obj.set('pred_classes', cls)
+    obj.set('pred_masks', masks)
+    obj.set('pred_boxes', boxes)
     return obj
-
 
 ################# MODEL PARAMS ##################
 
-video_path = "sample_0.mp4"
+video_path = 'sample_0.mp4'
 video = cv2.VideoCapture(video_path)
 width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -70,20 +67,13 @@ frames_per_second = video.get(cv2.CAP_PROP_FPS)
 num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
 # initialize video writer
-video_writer = cv2.VideoWriter(
-    "test_sample_0.mp4",
-    fourcc=cv2.VideoWriter_fourcc(*"mp4v"),
-    fps=float(frames_per_second),
-    frameSize=(width, height),
-    isColor=True,
-)
+video_writer = cv2.VideoWriter('test_sample_0.mp4', fourcc=cv2.VideoWriter_fourcc(*"mp4v"), fps=float(frames_per_second), frameSize=(width, height), isColor=True)
 
+# initialize predictor
 cfg = get_cfg()
 point_rend.add_pointrend_config(cfg)
-cfg.MODEL.DEVICE = "cpu"  # Ustawienie modelu w tryb CPU
-cfg.merge_from_file(
-    "projects\PointRend\configs\InstanceSegmentation\pointrend_rcnn_X_101_32x8d_FPN_3x_coco.yaml"
-)
+cfg.MODEL.DEVICE = 'cpu'  # Ustawienie modelu w tryb CPU
+cfg.merge_from_file("projects/PointRend/configs/InstanceSegmentation/pointrend_rcnn_X_101_32x8d_FPN_3x_coco.yaml")
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.95  # Set threshold for object detection
 cfg.MODEL.WEIGHTS = "detectron2://PointRend/InstanceSegmentation/pointrend_rcnn_X_101_32x8d_FPN_3x_coco/28119989/model_final_ba17b9.pkl"
 predictor = DefaultPredictor(cfg)
@@ -98,9 +88,8 @@ prev_frame = None
 prev_boxes = None
 prev_masks = None
 
-
 def runOnVideo(video, max_frames):
-    """Runs the predictor on every frame in the video (unless max_frames is given),
+    """ Runs the predictor on every frame in the video (unless max_frames is given),
     and returns the frame with the predictions drawn.
     """
 
@@ -116,41 +105,27 @@ def runOnVideo(video, max_frames):
 
         new_masks = modified_outputs.pred_masks
         new_boxes = modified_outputs.pred_boxes
-        indx_to_remove = []  # list of indexes of objects that are not moving
+        indx_to_remove = [] # list of indexes of objects that are not moving
 
-        if (
-            read_frames == 0
-        ):  # in first frame we cant detect moving objects because algorithm is based on previous frames
+        if read_frames == 0: # in first frame we cant detect moving objects because algorithm is based on previous frames 
             # updating previous frame features
             prev_frame = frame
             prev_masks = new_masks
             prev_boxes = new_boxes
         else:
-            for i in range(
-                len(new_masks)
-            ):  # iterate through every mask in current frame
-                jaccards = (
-                    []
-                )  # list of jaccard scores for pairs of mask in current frame and every mask in previous frame
-                new_mask_array = new_masks[
-                    i
-                ].numpy()  # converting to np.array for jaccard_score function input
-                found_in_prev = False  # flag in case if in previous frame corresponding mask was not detected
+            for i in range(len(new_masks)):# iterate through every mask in current frame
+                jaccards = [] # list of jaccard scores for pairs of mask in current frame and every mask in previous frame 
+                new_mask_array = new_masks[i].numpy() # converting to np.array for jaccard_score function input
+                found_in_prev = False # flag in case if in previous frame corresponding mask was not detected  
 
                 for j in range(len(prev_masks)):
-                    prev_mask_array = prev_masks[
-                        j
-                    ].numpy()  # converting to np.array for jaccard_score function input
+                    prev_mask_array = prev_masks[j].numpy() # converting to np.array for jaccard_score function input
                     # calculating jaccard score
-                    jaccards.append(
-                        jaccard_score(new_mask_array, prev_mask_array, average="micro")
-                    )  # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.jaccard_score.html
-                # print("MAX JACCARD SCORE: ", max(jaccards))
-                if max(jaccards) >= 0.5:  # threshold for jaccard score
-                    found_in_prev = True
-                    corresponding_mask_index = jaccards.index(
-                        max(jaccards)
-                    )  # highest jaccard score -> corresponding mask
+                    jaccards.append(jaccard_score(new_mask_array, prev_mask_array, average="micro")) # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.jaccard_score.html
+                #print("MAX JACCARD SCORE: ", max(jaccards))
+                if max(jaccards) >= 0.5: # threshold for jaccard score
+                    found_in_prev = True    
+                    corresponding_mask_index = jaccards.index(max(jaccards)) # highest jaccard score -> corresponding mask
 
                     n_x1 = new_boxes.tensor[i][0]
                     n_y1 = new_boxes.tensor[i][1]
@@ -176,41 +151,37 @@ def runOnVideo(video, max_frames):
                     pixels_r_prev = []
 
                     # iterate every pixel in smaller bounding box
-                    if area_p >= area_n:  # box got smaller
+                    if area_p >= area_n:# box got smaller
                         for y in range(int(n_y1.item()), int(n_y2.item())):
                             for x in range(int(n_x1.item()), int(n_x2.item())):
                                 # check if pixel is inside current mask and corresponding mask from previous frame
                                 pixel_in_mask = new_masks[i][y][x]
-                                pixel_in_prev_mask = prev_masks[
-                                    corresponding_mask_index
-                                ][y][x]
+                                pixel_in_prev_mask = prev_masks[corresponding_mask_index][y][x]
 
                                 if pixel_in_mask and pixel_in_prev_mask:
-                                    pixel = frame[y, x]
+                                    pixel = frame[y,x]             
                                     pixels_b.append(pixel[0])
                                     pixels_g.append(pixel[1])
                                     pixels_r.append(pixel[2])
-
-                                    pixel_prev = prev_frame[y, x]
+                                    
+                                    pixel_prev = prev_frame[y,x]
                                     pixels_b_prev.append(pixel_prev[0])
                                     pixels_g_prev.append(pixel_prev[1])
                                     pixels_r_prev.append(pixel_prev[2])
-                    else:  # box got bigger
+                    else:# box got bigger
                         for y in range(int(p_y1.item()), int(p_y2.item())):
                             for x in range(int(p_x1.item()), int(p_x2.item())):
                                 # check if pixel is inside current mask and in corresponding mask from previous frame
                                 pixel_in_mask = new_masks[i][y][x]
-                                pixel_in_prev_mask = prev_masks[
-                                    corresponding_mask_index
-                                ][y][x]
-
+                                pixel_in_prev_mask = prev_masks[corresponding_mask_index][y][x]
+                                
                                 if pixel_in_mask and pixel_in_prev_mask:
-                                    pixel = frame[y, x]
+                                    pixel = frame[y,x]
                                     pixels_b.append(pixel[0])
                                     pixels_g.append(pixel[1])
                                     pixels_r.append(pixel[2])
-
-                                    pixel_prev = prev_frame[y, x]
+                                    
+                                    pixel_prev = prev_frame[y,x]
                                     pixels_b_prev.append(pixel_prev[0])
                                     pixels_g_prev.append(pixel_prev[1])
                                     pixels_r_prev.append(pixel_prev[2])
@@ -235,11 +206,10 @@ def runOnVideo(video, max_frames):
 
         if read_frames:
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
+ 
             # Draw a visualization of the predictions using the video visualizer
-            visualization = v.draw_instance_predictions(
-                frame, modified_outputs.to("cpu")
-            )
+            visualization = v.draw_instance_predictions(frame, modified_outputs.to("cpu"))
+        
 
             # Convert Matplotlib RGB format to OpenCV BGR format
             visualization = cv2.cvtColor(visualization.get_image(), cv2.COLOR_RGB2BGR)
@@ -250,14 +220,14 @@ def runOnVideo(video, max_frames):
         if read_frames > max_frames:
             break
 
-
 # create a cut-off for debugging
 num_frames = 17
 
 # enumerate the frames of the video
 for visualization in tqdm.tqdm(runOnVideo(video, num_frames), total=num_frames):
+
     # write test image
-    cv2.imwrite("POSE detectron2.png", visualization)
+    cv2.imwrite('POSE detectron2.png', visualization)
 
     # write to video file
     video_writer.write(visualization)
